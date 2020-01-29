d606d5d1fe2d97adba65f69353ea9883beb2cefc
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-01-29 19:47:38 +0100

    tutorial slider and wip main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,20 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
+    path: 'help',
+    loadChildren: () => import('./tutorial/help/help.module').then( m => m.HelpPageModule)
+  },
+  {
     path: 'list/add',
     loadChildren: () => import('./addList/addList.module').then( m => m.AddListPageModule)
   },
   {
     path: 'list/:id',
     loadChildren: () => import('./todos-list/todos-list.module').then( m => m.TodosListPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./home/main/main.module').then( m => m.MainPageModule)
   }
 
 ];
