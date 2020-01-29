0521264b41a4f85da4b5c974128a5a7f32705eb1
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-01-29 20:34:27 +0100

    settings action sheet

--- src/app/home/main/main.page.ts
+++ src/app/home/main/main.page.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActionSheetController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.page.html',
-  styleUrls: ['./main.page.scss'],
+    selector: 'app-main',
+    templateUrl: './main.page.html',
+    styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+    constructor(public actionSheetController: ActionSheetController) {
+    }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Actions',
+      buttons: [{
+        text: 'Supprimer',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Partager',
+        icon: 'person-add',
+        handler: () => {
+          console.log('Partager clicked');
+        }
+      }, {
+        text: 'Epingler',
+        icon: 'pin',
+        handler: () => {
+          console.log('tâche epinglé');
+        }
+      }, {
+        text: 'Annuler',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Annuler clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
