79dd37428120a02554554891e5e9e6c483d096e3
DRIOWYA abdelghafour <abdodriowya@gmail.com>
2020-02-19 14:52:07 +0100

    reapply guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {Router} from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
     selector: 'app-root',
@@ -12,6 +13,7 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
     constructor(
+        private afAuth: AngularFireAuth,
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
@@ -28,7 +30,7 @@ export class AppComponent {
     }
 
     logout() {
-        alert('logout');
+        this.afAuth.auth.signOut();
         this.router.navigate(['login']);
     }
 }
