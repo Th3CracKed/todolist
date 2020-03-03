b0b058e9d8ce009568fbd9d7d7f64d1df369e7a4
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-03-03 19:35:46 +0100

    wip shared list first version toggle.

--- src/app/home/list-sharing/list-sharing.page.ts
+++ src/app/home/list-sharing/list-sharing.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Task} from '../../models';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ToastController} from '@ionic/angular';
 
 @Component({
     selector: 'app-list-sharing',
@@ -13,22 +14,33 @@ export class ListSharingPage implements OnInit {
     shareduser$: Observable<Task[]>;
     fakeListOfUsers = [{id: '0125545', email: 'todo@test.fr', canEdit: false}, {id: '81247654', email: 'canEdit@yes.fr', canEdit: true}];
 
-    constructor() {
+    constructor(private toastController: ToastController) {
     }
 
     addSharedUser = new FormGroup({
         newSharedUser: new FormControl('', [Validators.required, Validators.minLength(1)])
     });
 
+
+    async presentToast(msg: string, durationMs: number = 3000) {
+        const toast = await this.toastController.create({
+            message: msg,
+            duration: durationMs
+        });
+        toast.present();
+    }
+
     ngOnInit() {
     }
 
     deleteSharedUser(id: any) {
         // todo
+        this.presentToast('Delete user');
     }
 
     toogleCanEdit(user: { canEdit: boolean; id: string; email: string }) {
         // todo
+        this.presentToast('Can edit: ' + user.canEdit, 800);
     }
 
     addSharedUserToList() {
