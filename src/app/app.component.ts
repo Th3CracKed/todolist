8f8540c0c4f0a24eee295c15a33c9ebfb02f4e51
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-03-04 16:24:27 +0100

    hide navbar on specific route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {Router} from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
     selector: 'app-root',
@@ -12,6 +12,8 @@ import { AngularFireAuth } from '@angular/fire/auth';
     styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+    hideOnRoute = ['login', 'register', 'help'];
+
     constructor(
         private afAuth: AngularFireAuth,
         private platform: Platform,
@@ -32,5 +34,11 @@ export class AppComponent {
     logout() {
         this.afAuth.auth.signOut();
         this.router.navigate(['login']);
+    }
+
+    canDisplay() {
+        const routeName = this.router.url;
+        const routeName2 = routeName.substring(1);
+        return !(this.hideOnRoute.indexOf(routeName2) > -1);
     }
 }
