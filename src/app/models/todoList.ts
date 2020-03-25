aa3738f3bd21b98c7192203e0c708a43420b8f75
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-03-25 16:28:35 +0100

    add feature: pin list

--- src/app/models/todoList.ts
+++ src/app/models/todoList.ts
@@ -1,14 +1,16 @@
-
-export interface TodoList {    
-  id?: string;
-  title: string;
-  userId: string;
-  members?: Member;
+export interface TodoList {
+    id?: string;
+    title: string;
+    userId: string;
+    isPinned?: boolean;
+    members?: Member;
 }
 
-export interface Member { [userId: string]: CoreMember }
+export interface Member {
+    [userId: string]: CoreMember
+}
 
 export interface CoreMember {
-  email: string;
-  canEdit: boolean
+    email: string;
+    canEdit: boolean
 }
