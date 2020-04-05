dd4f240542dd1a3c5c03fb221ac117e1b88e49f2
Sylvestre WAMPACH <sylvestre.wampach@gmail.com>
2020-04-05 15:40:47 +0200

    [App theme] Global static app theme

--- src/app/home/main/main.module.ts
+++ src/app/home/main/main.module.ts
@@ -3,13 +3,19 @@ import { SharedModule } from 'src/app/shared.module';
 import { MainPageRoutingModule } from './main-routing.module';
 import { ClickStopPropagationDirective } from '../../directives/click-stop-propagation.directive';
 import { MainPage } from './main.page';
+import {ThemeComponent} from '../../themes/theme/theme.component';
 @NgModule({
-  imports: [
-    SharedModule,
-    MainPageRoutingModule
-  ],
-  declarations: [
-    MainPage,
-    ClickStopPropagationDirective]
+    imports: [
+        SharedModule,
+        MainPageRoutingModule
+    ],
+    exports: [
+        ThemeComponent
+    ],
+    declarations: [
+        MainPage,
+        ClickStopPropagationDirective,
+        ThemeComponent
+    ]
 })
 export class MainPageModule {}
