98ad05d8c91fd32109a3bf3047a52aa464680e09
DRIOWYA abdelghafour <abdodriowya@gmail.com>
2020-04-07 21:30:26 +0200

    fix fingerprint, thanks to native storage

--- src/app/auth/guards/auth-fingerprint.guard.ts
+++ src/app/auth/guards/auth-fingerprint.guard.ts
@@ -4,6 +4,7 @@ import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 import { Platform } from '@ionic/angular';
 import { FirebaseUtilsService } from 'src/app/services';
 import { take } from 'rxjs/operators';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class AuthFingerprintGuard implements CanActivate {
   constructor(private faio: FingerprintAIO,
     private platform: Platform,
     private firebaseUtilsService: FirebaseUtilsService,
-    private router: Router) { }
+    private router: Router,
+    private nativeStorage: NativeStorage) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
     return new Promise<boolean>(async resolve => {
@@ -25,8 +27,9 @@ export class AuthFingerprintGuard implements CanActivate {
           return;
         }
         if (this.platform.is('cordova')) {
-          const isFirstLogin = window.localStorage.getItem('first_login');
-          if (!!isFirstLogin) {
+
+          const isFirstLogin = await this.nativeStorage.getItem('first_login');
+          if (isFirstLogin) {
             resolve(true);
             return;
           }
@@ -37,6 +40,11 @@ export class AuthFingerprintGuard implements CanActivate {
           }
           const isAuthorized = await this.faio.show({ title: 'Security check', disableBackup: true });
           if (!!isAuthorized) {
+            try {
+              await this.nativeStorage.setItem('first_login', true);
+            } catch (error) {
+              console.error('Error storing item', error);
+            }
             resolve(true);
           } else {
             this.router.navigate(['/login']);
