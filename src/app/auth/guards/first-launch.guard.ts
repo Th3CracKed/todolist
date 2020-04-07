a779ce200f88feccb051c0f9c9c386dfba96927c
DRIOWYA abdelghafour <abdodriowya@gmail.com>
2020-04-07 22:00:30 +0200

    use ionic storage instead of local storage for help guard

--- src/app/auth/guards/first-launch.guard.ts
+++ src/app/auth/guards/first-launch.guard.ts
@@ -1,29 +1,44 @@
-import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
-import {FingerprintAIO} from '@ionic-native/fingerprint-aio/ngx';
-import {Platform} from '@ionic/angular';
-import {FirebaseUtilsService} from 'src/app/services';
-import {take} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { Platform } from '@ionic/angular';
 
 @Injectable({
     providedIn: 'root'
 })
 export class FirstLaunchGuard implements CanActivate {
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+        private platform: Platform,
+        private nativeStorage: NativeStorage) {
     }
 
-    canActivate(): boolean {
-        try {
-            const firstLaunch = window.localStorage.getItem('isFirstLaunch');
-            if (!firstLaunch) {
-                // show help page if first launch
-                window.localStorage.setItem('isFirstLaunch', 'set');
-                this.router.navigate(['/help']);
+    canActivate(): Promise<boolean> {
+        console.log('FirstLaunchGuard');
+        return new Promise<boolean>(async resolve => {
+            try {
+                if (this.platform.is('cordova')) {
+                    console.log('FirstLaunchGuard here 1 ');
+                    await this.nativeStorage.getItem('isFirstLaunch');
+                    return resolve(true);
+                } else {
+                    const firstLaunch = window.localStorage.getItem('isFirstLaunch');
+                    if (!firstLaunch) {
+                        // show help page if first launch
+                        window.localStorage.setItem('isFirstLaunch', 'set');
+                        this.router.navigate(['/help']);
+                    }
+                    return resolve(true);
+                }
+            } catch {
+                try {
+                    await this.nativeStorage.setItem('isFirstLaunch', true);
+                    this.router.navigate(['/help']);
+                } catch (error) {
+                    console.error('Error storing item', error);
+                    this.router.navigate(['/help']);
+                }
             }
-            return true;
-        } catch {
-            return true;
-        }
+        });
     }
 }
