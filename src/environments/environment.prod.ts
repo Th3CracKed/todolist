2c7e491086ce00f3f914288925a1efc069566197
DRIOWYA abdelghafour <abdodriowya@gmail.com>
2020-04-13 11:59:17 +0200

    add env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: false,
+  production: true,
   googleWebClientId: '201259008916-6t19tnn8a88kmeeage0vprvn4t6vbto8.apps.googleusercontent.com',
   actionCodeSettings: {
     // Your redirect URL
@@ -27,3 +31,12 @@ export const firebaseConfig = {
   appId: "1:201259008916:web:85afd7c2943a39f4258581",
   measurementId: "G-616NKZL5XG"
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
